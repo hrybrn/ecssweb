e5049db50673f65a06202617c6d954e44ec49013
norayna <rayna@bozhkova.info>
2017-06-17 01:00:28 +0300

    i got issues and u got em too

--- index.php
+++ index.php
@@ -17,11 +17,11 @@ include_once("navbar.php");
             <h1>Welcome to ECSS!</h1>
 
             <h2>
-                Welcome to the Electronics and Computer Science (ECS) Society website. 
-                We are a student-run society for ECS at the University of Southampton. 
-                On this website you will find information about the society such as who we are, what we aim to do, as well as what events we are currently organising! 
-                Still reading? You can even find out more about us.
-                To keep up with the latest events, join the ECSS Facebook group and follow us on Twitter, @ECS_Society.
+                <p>Welcome to the Electronics and Computer Science (ECS) Society website. </p>
+                <p>We are a student-run society for ECS at the University of Southampton. </p>
+                <p>On this website you will find information about the society such as who we are, what we aim to do, as well as what events we are currently organising! </p>
+                <p>Still reading? You can even find out more about us.</p>
+                <p>To keep up with the latest events, join the ECSS Facebook group and follow us on Twitter, @ECS_Society.</p>
             </h2>
         </div>
     </div>
