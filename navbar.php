583ac6f1cbe423981b35f8a1f8caa56f6092eadc
norayna <rayna@bozhkova.info>
2017-06-17 22:12:09 +0300

    made questionable yet more presentable navbar

--- navbar.php
+++ navbar.php
@@ -4,16 +4,44 @@ function getNavBar() {
     global $relPath;
     $nav = '<script src="' . $relPath . 'navbar.js"></script>';
     $nav .= '<script src="' . $relPath . 'jquery.js"></script>';
-    $nav .= '<link rel="stylesheet" href="' . $relPath . 'theme.css">';
+    $nav .= '<link rel="stylesheet" href="' . $relPath . 'navbar.css">';
     $raw = file_get_contents($relPath . "data/links.json");
     $links = json_decode($raw, true);
     
-    $nav .= '<ul class="mainNav">';
+    $nav .= '<div class="navbar">';
     
-    $child = '<div id="child" hidden="true"><ul class="childNav">';
+    $nav .= 
+     '<div class="dropdown">
+  <a class="dropbtn">Dropdown</a>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div> ';
+    
+    //$nav .= '<ul class="mainNav">';
+    
+    //$child = '<div id="child" hidden="true"><ul class="childNav">';
     
     foreach ($links as $name => $address) {
         if(is_array($address)){
+             $nav .= 
+                '
+                 <div class="dropdown">
+                 <button class="dropbtn">Dropdown</button>
+                 <div class="dropdown-content">';
+             
+            foreach($address as $page => $link){
+                $nav .= '<a href="' . $relPath . $link . '">' . $page . '</a>';
+            }
+            $nav .= '</div></div>';
+        } 
+        else {
+            $nav .= '<button>Button</button>'; 
+        }
+        
+        /*if(is_array($address)){
             $nav .= '<li><a onclick="showDropdown()">' . $address['name'] . '</a></li>';
             $nav .= '<div id="myDropdown" class="dropdown-content">';
             
@@ -28,7 +56,8 @@ function getNavBar() {
         else {
             $nav .= '<li><a href="' . $relPath . $address . '">' . $name . '</a></li>';
         }
-        
+        */
     }
-    return $nav . "</ul>" . $child;
+    //return $nav . "</ul>" . $child;
+    return $nav . "</div><br>";
 }
