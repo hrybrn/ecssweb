18ad0d3f2083035783f491d5df33e6523982ae93
hrybrn <harryjgbrown@gmail.com>
2017-06-17 14:14:04 +0100

    round buttons

--- societies/societies.php
+++ societies/societies.php
@@ -9,10 +9,12 @@ echo getNavBar();
 $raw = file_get_contents($relPath . "data/societies.json");
 $societies = json_decode($raw, true);
 
+echo '<div class="buttonGroup">';
 $i = 0; 
 foreach($societies as $society => $data){
     echo '<button onclick="showMember(' . $i . ')" id="button' . $i++ . '">' . $society . '</button>';
 }
+echo '</div>';
 
 ?>
 <script> var relPath = "<?= $relPath ?>"; </script>
