2ad8fff8ca5f36c0a3080a8716bffa3814098a19
hrybrn <harryjgbrown@gmail.com>
2017-06-19 21:46:45 +0100

    pretty buttons!

--- committee/committee.php
+++ committee/committee.php
@@ -9,10 +9,14 @@ echo getNavBar();
 $raw = file_get_contents($relPath . "data/committee.json");
 $committee = json_decode($raw, true);
 
+echo '<div class="buttonGroup">';
+
 $i = 0; 
 foreach($committee as $member => $data){
     echo '<button onclick="showMember(' . $i . ')" id="button' . $i++ . '">' . $member . '</button>';
 }
+
+echo '</div>';
 
 ?>
 <script> var relPath = "<?= $relPath ?>"; </script>
@@ -21,9 +25,15 @@ foreach($committee as $member => $data){
 <link rel="stylesheet" href="<?= $relPath ?>theme.css">
 <body>
     <br>
-    <img id="memberImage" width="300"/>
-    <table id="memberTable"></table>
-    
+    <table>
+        <tr>
+            <td>
+                <img id="memberImage" width="400" style="padding: 5px;"/>
+            </td>
+            <td>
+                <table id="memberTable"></table>
+            </td>
+    </table>
     <script>
         $(document).ready( function(){
             showMember("0");
