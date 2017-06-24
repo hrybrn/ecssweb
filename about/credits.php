94d51e2d499c3727b22c2debc145005c589badd6
chriscoles01 <cbc1g16@soton.ac.uk>
2017-06-24 01:27:44 +0100

    added chris to credits
    yea i commited finally... added myself to credits

--- about/credits.php
+++ about/credits.php
@@ -11,10 +11,12 @@ echo getNavBar();
         <tr>
             <td align="center"><img src="<?= $relPath ?>images/harry.jpg" width="400" ></td>
             <td align="center"><img src="<?= $relPath ?>images/rayna.jpg" height="400"></td>
+            <td align="center"><img src="<?= $relPath ?>images/chris.jpg" height="400"></td>
         </tr>
         <tr>
             <td>Harry wrote most of the website and set up the system.</td>
             <td>Rayna wrote most of the CSS and made this website pretty af.</td>
+            <td>Chris has yet to make a commit, he will one day once he learns PHP.</td>
         </tr>
     </table>
 </html>
