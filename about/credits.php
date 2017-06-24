e8eb8cc3acc3657a8ca6824892e8143593db144a
hrybrn <harryjgbrown@gmail.com>
2017-06-24 11:15:55 +0100

    again adding stuff to the credits

--- about/credits.php
+++ about/credits.php
@@ -8,14 +8,14 @@ echo getNavBar();
     <link rel="stylesheet" href="../theme.css">
 
     <table align="center">
-        <tr><td colspan="2"><h3 align="center">Production Credits</h3></td></tr>
+        <tr><td colspan="3"><h3 align="center">Production Credits</h3></td></tr>
         <tr>
-            <td align="center"><img src="<?= $relPath ?>images/harry.jpg" width="400" ></td>
-            <td align="center"><img src="<?= $relPath ?>images/rayna.jpg" height="400"></td>
-            <td align="center"><img src="<?= $relPath ?>images/chris.jpg" height="400"></td>
+            <td class="creditImage"><img src="<?= $relPath ?>images/harry.jpg" width="400" ></td>
+            <td class="creditImage"><img src="<?= $relPath ?>images/rayna.jpg" height="400"></td>
+            <td class="creditImage"><img src="<?= $relPath ?>images/chris.jpg" height="400"></td>
         </tr>
         <tr>
-            <td>Harry wrote most of the website and set up the system.</td>
+            <td>Harry wrote the pages and made other people format them.</td>
             <td>Rayna wrote most of the CSS and made this website pretty af.</td>
             <td>Chris has yet to make a commit, he will one day once he learns PHP.</td>
         </tr>
