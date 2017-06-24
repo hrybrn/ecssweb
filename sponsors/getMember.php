8afedb45b8d1a9c9f44c3e5be5d52262e13df8d3
hrybrn <harryjgbrown@gmail.com>
2017-06-24 23:23:37 +0100

    type sent through of sponsors

--- sponsors/getMember.php
+++ sponsors/getMember.php
@@ -12,6 +12,7 @@ $sponsors = json_decode($raw, true);
 foreach($sponsors as $type => $sponsor) {
     foreach($sponsor as $title => $data)
     if($title == $name) {
+        $data['Type'] = $type;
         echo json_encode($data);
         return;
     }
