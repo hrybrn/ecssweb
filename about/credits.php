e9c9a52623b34d4c9255fbd3032f93a1d4ed6b9f
hrybrn <harryjgbrown@gmail.com>
2017-06-24 01:02:55 +0100

    drunk credit changes

--- about/credits.php
+++ about/credits.php
@@ -6,8 +6,8 @@ include_once($relPath . "navbar/navbar.php");
 echo getNavBar();
 ?>
 <html>
-    <h3>Production Credits</h3>
-    <table>
+    <table align="center">
+        <tr><td colspan="2"><h3 align="center">Production Credits</h3></td></tr>
         <tr>
             <td align="center"><img src="<?= $relPath ?>images/harry.jpg" width="400" ></td>
             <td align="center"><img src="<?= $relPath ?>images/rayna.jpg" height="400"></td>
