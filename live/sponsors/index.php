07e8e87e0ef48cdeb3301901c0787e4fa9c8e33c
cjx <i@cjxol.com>
2017-07-16 23:46:13 +0800

    Sponsors page is now table free

--- live/sponsors/index.php
+++ live/sponsors/index.php
@@ -43,7 +43,7 @@ foreach ($sponsors['Bronze'] as $sponsor => $data) {
 <div id="sponsorsPageContainer">
     <div id="sponsorImageContainer"><img id="sponsorImage" class="pageImage"/></div>
     <div id="sponsorButtonGroup"><?= $gold; $silver; $bronze ?></div>
-    <table id="sponsorTable" class="pageTable"></table>
+    <div id="sponsorTable"></div>
 </div>
 
 <script>
