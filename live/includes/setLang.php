148cd3e7bb4c98c6956df7804edc161a3e77e676
cjx <i@cjxol.com>
2017-07-21 17:07:57 +0800

    edited setLang.php

--- live/includes/setLang.php
+++ live/includes/setLang.php
@@ -2,6 +2,9 @@
 function setLang() {
     global $lang;
     global $relPath;
+    if (!isset($relPath)) {
+        $relPath = '../';
+    }
     $availableLocales = scandir($relPath . '../data/locale');
     if (isset($_GET['lang'])) { // get language setting from url
         foreach ($availableLocales as $availableLocale) { // check if valid lang
@@ -28,14 +31,59 @@ function setLang() {
                 $acceptLanguage = strtolower($acceptLanguage);
                 if (strcmp($acceptLanguage, $availableLocale) === 0) {
                     $lang =  $availableLocale;
-                    // TODO: perform a redirect
+                    redirectToLang($lang);
                     return;
                 }
             }
         }
     }
     $lang = 'en';
-    // TODO: perform a redirect
+    redirectToLang($lang);
+}
+
+function redirectToLang($lang) {
+    // scheme
+    $scheme = isset($_SERVER['HTTPS']) ? 'https' : 'http';
+    // query
+    $_GET['lang'] = $lang;
+    $query = http_build_query($_GET);
+    // path
+    $path = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+    // assemble url
+    $url = $scheme . '://' . $_SERVER['HTTP_HOST'] . $path . '?' . $query;
+
+    // redirect
+    header('Location: ' . $url);
+    exit;
 }
 
 setLang();
+
+// set locale
+$lc = str_replace('-', '_', $lang);
+$count = preg_match('/^([a-z]{2})_([a-z]{2})$/i', $lc, $match);
+if ($count === 1) {
+    $lc = strtolower($match[1]) . '_' . strtoupper($match[2]);
+}
+
+putenv("LANG=$lc");
+setlocale(LC_ALL, $lc);
+
+$domain = 'messages';
+bindtextdomain($domain, $relPath . '../data/locale');
+bind_textdomain_codeset($domain, 'UTF-8');
+textdomain($domain);
+
+// example
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?= _("hi") ?></title>
+</head>
+<body>
+<?= _("hi") ?>
+</body>
+</html>
