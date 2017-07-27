a9f57e706832fd3f336baf3642eab55b3510b24e
norayna <rayna@bozhkova.info>
2017-07-27 23:40:14 +0100

    lit.gif

--- live/navbar/navbar.php
+++ live/navbar/navbar.php
@@ -17,7 +17,8 @@ function getNavBar() {
     $links = json_decode($raw, true);
     
     $nav .= '<div class="navbar">';
-    $nav .= '<img id="navbarLogo" src="' . $relPath . 'images/new-logo-white-transparent-ver2.png" height="60">';
+    //$nav .= '<img id="navbarLogo" src="' . $relPath . 'images/new-logo-white-transparent-ver2.png" height="60">';
+    $nav .= '<img id="navbarLogo" src="' . $relPath . 'images/lit.gif" height="60">';
     
     /*
      * Every entry in $links contains an element of the navbar (link & address).
