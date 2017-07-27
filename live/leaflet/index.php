1cb231579640065317af91a1f1f2066e3980fa76
norayna <rayna@bozhkova.info>
2017-07-27 22:38:47 +0100

    removed all of harrys efforts and redid leaflet page, not completed yet

--- live/leaflet/index.php
+++ live/leaflet/index.php
@@ -1,12 +1,11 @@
 <?php
-
 $relPath = "../";
 
 include_once($relPath . "navbar/navbar.php");
 
-if(!isset($_GET['section'])) {
-	echo "Not a valid leaflet section!";
-	exit;
+if (!isset($_GET['section'])) {
+    echo "Not a valid leaflet section!";
+    exit;
 }
 $section = $_GET['section'];
 
@@ -16,27 +15,70 @@ $sectionData = json_decode($raw, true);
 $sectionData = $sectionData[$section];
 
 $body = "";
-for($i = 0; $i < count($sectionData['text']); $i++){
-	if(in_array($i, $sectionData['titleIndexes'])){
-		$body .= "<h3>" . $sectionData['text'][$i] . "</h3>";
-	} else {
-		$body .= "<p>" . $sectionData['text'][$i] . "</p>";
-	}
 
-	if(isset($sectionData['images'][$i])){
-		$body .= "<img class='leafletImage' src='" . $relPath . $sectionData['images'][$i] . "'>";
-	}
+//todo: print out title
+$body .= "<div id=\"titleDiv\">";
+$body .= "<h1 id=\"sectionTitle\">" . $section . "</h1>";
+
+foreach ($sectionData as $key => $value) {
+    if ($key === "Overview") {
+        //todo: print out overview
+        $body .= "<p id=\"overview\">" . $value . "</p>";
+        $body .= "</div>";
+    } else
+    if ($key === "Subsections") {
+        //$body .= "<p>lolool</p>";
+        foreach ($value as $subkey => $subvalue) {
+            //$body .= "<p>sub-lolool</p>";
+            //todo: print out subsection title
+            $body .= "<div class=\"subsection\">";
+            $body .= "<div class=\"subsectionText\">";
+            $body .= "<h1 class=\"subsectionTitle\">" . $subkey . "</h1>";
+
+            foreach ($subvalue as $subsubkey => $subsubvalue) {
+                if ($subsubkey == "Text") {
+                    //$body .= "<p>subsub-lolool</p>";
+                    //todo: print out text
+                    $body .= "<p class=\"subsectionText\">" . $subsubvalue . "</p>";
+                    $body .= "</div>";
+                }
+
+                if ($subsubkey == "Image") {
+                    //$body .= "<p>sub-image-lolool</p>";
+                    //todo: print out image
+                    $body .= "<img class=\"subsectionImage\" src=\"" . $subsubvalue . "\">";
+                }
+            }
+            
+            $body .= "</div>";
+        }
+    }
 }
+
+/*
+  for($i = 0; $i < count($sectionData['text']); $i++){
+  if(in_array($i, $sectionData['titleIndexes'])){
+  $body .= "<h3>" . $sectionData['text'][$i] . "</h3>";
+  } else {
+  $body .= "<p>" . $sectionData['text'][$i] . "</p>";
+  }
+
+  if(isset($sectionData['images'][$i])){
+  $body .= "<img class='leafletImage' src='" . $relPath . $sectionData['images'][$i] . "'>";
+  }
+  }
+
+ */
 ?>
 <html>
-	<title><?= $sectionData['title'] ?></title>
+    <title><?= $sectionData['title'] ?></title>
 
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="<?=$relPath?>theme.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="<?= $relPath ?>theme.css"/>
     <link rel="stylesheet" type="text/css" href="/leaflet/leaflet.css"/>
 
     <body>
-        <?=getNavBar();?>
-        <?=$body?>
+<?= getNavBar(); ?>
+<?= $body ?>
     </body>
 </html>
--- live/navbar/navbar.php
+++ live/navbar/navbar.php
@@ -51,10 +51,10 @@ function getNavBar() {
     //$nav .= '<div id="innerLangMenu">';
     
     //DISABLED LANGUAGE ICONS
-    /*
+    
     $nav .= '<a href="' . $nakedURL . '?lang=bg" target="_self"><img class="langIcon" id="notCurrentLangIcon" src="' . $relPath . '/images/flag-icons/bg.png" height="20"></a>';
     $nav .= '<a href="' . $nakedURL . '?lang=zh-cn" target="_self"><img class="langIcon" id="notCurrentLangIcon" src="' . $relPath . '/images/flag-icons/zh-cn.png" height="20"></a>';
-     */
+     
     
     
     $nav .= '</div>';
