c176477ac33776603ea18941ede0704ec1bcba35
cjx <i@cjxol.com>
2017-09-03 12:25:30 +0800

    indent navbar.php code

--- live/navbar/navbar.php
+++ live/navbar/navbar.php
@@ -9,17 +9,18 @@ function getNavBar() {
     global $relPath;
     global $lang;
     global $nakedURL;
-    
+
     $nav = "";
     $nav .= '<script src="' . $relPath . 'jquery.js"></script>'; // Javascript
     $nav .= '<link rel="stylesheet" href="' . $relPath . 'navbar/navbar.css">'; // css
+
     $raw = file_get_contents($relPath . "../data/en/links.json");
     $links = json_decode($raw, true);
-    
+
     $nav .= '<div class="navbar">';
     //$nav .= '<img id="navbarLogo" src="' . $relPath . 'images/new-logo-white-transparent-ver2.png" height="60">';
     $nav .= '<img id="navbarLogo" src="' . $relPath . 'images/new-logo-black.png" height="60">';
-    
+
     /*
      * Every entry in $links contains an element of the navbar (link & address).
      * For elements with a dropdown, the address is an array of elements for the dropdown.
@@ -29,23 +30,19 @@ function getNavBar() {
     foreach ($links as $name => $address) {
         if(is_array($address)){
             if(strpos($address['default'], "lang") === false){
-             $nav .=
-                '
-                 <div class="dropdown">
-                 <a id="navButton" href="'. $relPath . $address['default'] . '?lang=' . $lang . '">' . _($name) . '</a>
-                 
+                $nav .= '
+                <div class="dropdown">
+                <a id="navButton" href="'. $relPath . $address['default'] . '?lang=' . $lang . '">' . _($name) . '</a>
                 <div class="dropdown-content">';
             } else {
-                $nav .=
-                '
-                 <div class="dropdown">
-                 <a id="navButton" href="'. $relPath . $address['default'] . '">' . _($name) . '</a>
-                 
+                $nav .= '
+                <div class="dropdown">
+                <a id="navButton" href="'. $relPath . $address['default'] . '">' . _($name) . '</a>
                 <div class="dropdown-content">';
             }
-             
+
             unset($address['default']);
-            
+
             foreach($address as $page => $link){
                 if(strpos($link, "lang") === false){
                     $nav .= '<a class="link" href="' . $relPath . $link . '?lang=' . $lang . '">' . _($page) . '</a>';
@@ -54,27 +51,26 @@ function getNavBar() {
                 }
             }
             $nav .= '</div></div>';
-        } 
-        else {
-            $nav .= 
-                '<a id="navButton" href="'. $relPath . $address . '?lang=' . $lang . '">' . _($name) . '</a>';
         }
-        
+        else {
+            $nav .= '<a id="navButton" href="'. $relPath . $address . '?lang=' . $lang . '">' . _($name) . '</a>';
+        }
+
     }
     $nav .= '<div id="langMenu">';
     $nav .= '<a href="' . $nakedURL . '?lang=en"" target="_self"><img class="langIcon" id="currentLangIcon" src="' . $relPath . '/images/flag-icons/en.png" height="20"></a>';
     //$nav .= '<div id="innerLangMenu">';
-    
+
     //DISABLED LANGUAGE ICONS
     /*
     $nav .= '<a href="' . $nakedURL . '?lang=bg" target="_self"><img class="langIcon" id="notCurrentLangIcon" src="' . $relPath . '/images/flag-icons/bg.png" height="20"></a>';
     $nav .= '<a href="' . $nakedURL . '?lang=zh-cn" target="_self"><img class="langIcon" id="notCurrentLangIcon" src="' . $relPath . '/images/flag-icons/zh-cn.png" height="20"></a>';
     */
-    
-    
+
+
     $nav .= '</div>';
-    
-    
+
+
     //DISABLED SHOP BUTTON
 //$nav .= '
     //    <div id="shopButtonDiv">
