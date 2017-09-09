4461464bf8f58831a5682eb90279afba93567efb
Unknown <harryjgbrown@gmail.com>
2017-09-09 16:49:39 +0100

    changed location of table creation scripts and changed voting page

--- live/voting/index.php
+++ live/voting/index.php
@@ -1,15 +1,32 @@
 <?php
 $relPath = "../";
 
-$raw = file_get_contents($relPath . "../data/setup.json");
-$setup = json_decode($raw, true);
+$dbLoc = realpath($relPath . "../db/ecss.db");
 
-if($setup['phdMasters'] == "disabled")
-    $mode = "general";
-else
-    $mode = "phdMasters";
+$db = new PDO('sqlite:' . $dbLoc);
 
-$mode = $setup[$mode];
+//check for the existence of a election that is in nomination phase
+$sql = "SELECT *
+		FROM election AS e
+		WHERE datetime(e.nominationStartDate) < datetime(now)
+		AND datetime(e.nominationEndDate) > datetime(now);";
 
-//including relevant layout
-header( 'Location: http://society.ecs.soton.ac.uk/voting/' . $mode . '.php' ) ;
+if(!$res = $db->query($sql)){
+	//no current nomination phase, checking for voting phase
+	$sql = "SELECT *
+			FROM election AS e
+			WHERE datetime(e.votingStartDate) < datetime(now)
+			AND datetime(e.votingEndDate) > datetime(now);";
+
+	$voting = true;
+	
+	if(!$res = $db->query($sql)){
+		//no current election is happening
+		exit;
+	}
+}
+
+//$res is now the db result object
+//get the election object
+$election = $res->fetchObject();
+
