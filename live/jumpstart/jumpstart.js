4a32c8a842918dd818b7a8cae7de88f63c38241f
Unknown <harryjgbrown@gmail.com>
2017-09-10 22:10:48 +0100

    group table added!!
    css needs some more work but the ajax call works

--- live/jumpstart/jumpstart.js
+++ live/jumpstart/jumpstart.js
@@ -1,14 +1,16 @@
+var tableVisible = 0;
+
 function load(){
 	var boxes = {};
 
 	var highest = 0;
 
 	$(groups).each(function(){
-		var box = "<div class=\"overviewSquare\">";
+		var box = "<a onclick='openGroup(" + this.groupID + ")'><div id='group" + this.groupID + "' class=\"overviewSquare\">";
 		box += "<image src='../images/" + this.image + "'></image>";
                 box += "<h3>Group " + this.groupID + "</h3>";
 		box += "<p><marquee>" + this.name + "</marquee></p>";
-		box += "</div>";
+		box += "</div></a>";
 
 		boxes[this.groupID] = box;
 
@@ -19,4 +21,36 @@ function load(){
 	for(var i=0;i<=highest;i++)
 		if(boxes[i] != "")
 			$('body').append(boxes[i]);
+}
+
+function openGroup(groupID){
+
+	$('.groupTable').remove();
+	if(tableVisible == groupID){
+		tableVisible = 0;
+	} else {
+		tableVisible = groupID;
+		$.ajax({
+			url: 'getGroup.php',
+	        type: 'get',
+	        data: {'groupID': groupID},
+	        dataType: 'json',
+	        success: function (group) {
+	        	var html = "<div class='groupTable'><table><tr><th>Name</th><th>Role</th></tr>";
+
+	        	$(group.helpers).each(function(){
+	        		html += "<tr><td>" + this.name + "</td><td>Helper</td></tr>";
+	        	});
+
+	        	$(group.freshers).each(function(){
+	        		html += "<tr><td>" + this.name + "</td><td>Fresher</td></tr>";
+	        	});
+
+	        	html += "</table></div>";
+
+	        	$('#group' + groupID).append(html);
+	        	tableVisible[groupID] = true;
+	        }
+	    });
+	}
 }
