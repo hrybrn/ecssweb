8f5eb7239687a386d9af333b9711139af9be3bcf
cjx <i@cjxol.com>
2017-09-13 22:02:28 +0800

    New slideshow in Sports page

--- live/leaflet/index.php
+++ live/leaflet/index.php
@@ -60,7 +60,7 @@ foreach ($sectionData as $key => $value) {
                         }
                     }
 
-                    echo "
+                    $body .= "
                         <script>
                             var files = " . json_encode($files) . ";
                             var section = '" . $section . "';
@@ -73,8 +73,10 @@ foreach ($sectionData as $key => $value) {
                         $imageClass = "subsectionImage";
                     }
 
-                    $body .= "<div class=\"slideshow " . $imageClass . "\">";
-                    $body .= "<button class='slideshowButton' onclick='nextPhoto(-1)'>Previous</button><button class='slideshowButton' onclick='nextPhoto(1)'>Next</button></div>";
+                    $slideshowId = "slideshow" . $subkey;
+                    $body .= "<div id=\"" . $slideshowId . "\" class=\"slideshow " . $imageClass . "\">";
+                    $body .= "</div>";
+                    $body .= "<script>$(document).ready(function() { var slideshow = new Slideshow(document.getElementById(\"" . $slideshowId . "\"), files); });</script>";
                 }
             }
             
@@ -83,24 +85,18 @@ foreach ($sectionData as $key => $value) {
     }
 }
 ?>
+<!doctype html>
 <html>
+<meta charset="utf-8">
     <title><?= $sectionData['title'] ?></title>
-    
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="<?= $relPath ?>theme.css"/>
     <link rel="stylesheet" type="text/css" href="<?= $relPath ?>/leaflet/leaflet.css"/>
-    <script src="<?=$relPath ?>leaflet/leaflet.js"></script>
-
+    <link rel="stylesheet" href="<?= $relPath ?>static/slideshow.css">
+    <script src="<?= $relPath ?>jquery.js"></script>
+    <script src="<?= $relPath ?>static/slideshow.js"></script>
     <body>
 <?= getNavBar(); ?>
 <?= $body ?>
     </body>
-
-    <script>
-        $(document).ready(function(){
-            if(typeof files !== 'undefined'){
-                nextPhoto(0);
-            }
-        });
-    </script>
 </html>
