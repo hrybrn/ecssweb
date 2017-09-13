a2da2ca29f60d6e71e470b923d79d27053273010
cjx <i@cjxol.com>
2017-09-13 19:27:11 +0800

    Huge refactor on slideshow, make it easy to create a slideshow anywhere

--- live/about/index.php
+++ live/about/index.php
@@ -13,7 +13,9 @@ include_once ($relPath . 'includes/setLang.php');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?= $relPath ?>theme.css">
     <link rel="stylesheet" href="about.css">
-    <script src="about.js"></script>
+    <link rel="stylesheet" href="<?= $relPath ?>static/slideshow.css">
+    <script src="<?= $relPath ?>jquery.js"></script>
+    <script src="<?= $relPath ?>static/slideshow.js"></script>
 </head>
 <body>
 <?php
@@ -25,12 +27,6 @@ echo getNavBar();
 
     <div id="slideshow" class="slideshow">
         <noscript>Javascript is required for the slideshow.</noscript>
-        <div class="slideshowImageContainer"><img src="<?= $relPath ?>images/people.jpg" alt=""></div>
-        <div class="slideshowControl">
-            <button class="slideshowButton" onclick="nextPhoto(-1)">&lt;</button>
-            <button class="slideshowButton" onclick="slideshowPausePlay(0, this)">||</button>
-            <button class="slideshowButton" onclick="nextPhoto(1)">&gt;</button>
-        </div>
     </div>
 
     <h1>About ECSS</h1>
@@ -49,5 +45,11 @@ echo getNavBar();
         Our most recent Constitution, SUSU Society Page, our latest Election Results and the subsequent pages on this website are available for your immediate browsing. If you are a UoS Student, you can also log into the portal for current merchandise orders, voting and more. If at any point you require any more information, or are interested in sponsoring our society, please do not hesitate to contact us.
     </p>
 </section>
+<script>
+    var files = ["..\/images\/leaflet\/football\/slideshow\/0000000000000000000000000000000000.jpg","..\/images\/leaflet\/football\/slideshow\/20403740_1114381945329551_1864349788_n.jpg","..\/images\/leaflet\/football\/slideshow\/20427173_1114388448662234_883674683_o.jpg","..\/images\/leaflet\/football\/slideshow\/20496532_1114381898662889_1733044866_n.jpg","..\/images\/leaflet\/football\/slideshow\/20503787_1114381985329547_499884642_n.jpg","..\/images\/leaflet\/football\/slideshow\/20504170_1114381918662887_326041608_n.jpg","..\/images\/leaflet\/football\/slideshow\/20524467_1114387758662303_1237932208_o.jpg","..\/images\/leaflet\/football\/slideshow\/20526397_1114381921996220_1916863868_n.jpg","..\/images\/leaflet\/football\/slideshow\/20526431_1114381961996216_446743077_n.jpg","..\/images\/leaflet\/football\/slideshow\/20526460_1114381955329550_474103275_n.jpg","..\/images\/leaflet\/football\/slideshow\/20526571_1114381981996214_1946866164_n.jpg","..\/images\/leaflet\/football\/slideshow\/20526700_1114381941996218_1615201612_n.jpg","..\/images\/leaflet\/football\/slideshow\/20527337_1114381965329549_231454038_n.jpg","..\/images\/leaflet\/football\/slideshow\/20527435_1114381968662882_1854301463_n.jpg","..\/images\/leaflet\/football\/slideshow\/20527444_1114374405330305_615881216_n.jpg","..\/images\/leaflet\/football\/slideshow\/20527520_1114381951996217_190344425_n.jpg","..\/images\/leaflet\/football\/slideshow\/20536118_1114388478662231_402331591_o.jpg","..\/images\/leaflet\/football\/slideshow\/20536253_1114388108662268_60028155_o.jpg","..\/images\/leaflet\/football\/slideshow\/20542998_1114388118662267_507013736_o.jpg","..\/images\/leaflet\/football\/slideshow\/20561660_1114381948662884_1996504012_n.jpg","..\/images\/leaflet\/football\/slideshow\/20561663_1114381911996221_1849869183_n.jpg","..\/images\/leaflet\/football\/slideshow\/20561724_1114381908662888_232895760_n.jpg","..\/images\/leaflet\/football\/slideshow\/20561743_1114381915329554_274723609_n.jpg","..\/images\/leaflet\/football\/slideshow\/20562656_1114381895329556_240921432_n.jpg","..\/images\/leaflet\/football\/slideshow\/20562831_1114381901996222_164283219_n.jpg","..\/images\/leaflet\/football\/slideshow\/20590595_1114388061995606_1488909998_o.jpg","..\/images\/leaflet\/football\/slideshow\/20590614_1114387768662302_1125732714_o.jpg","..\/images\/leaflet\/football\/slideshow\/20590858_1114388425328903_1832757774_o.jpg","..\/images\/leaflet\/football\/slideshow\/20597555_1114388441995568_291814333_o.jpg","..\/images\/leaflet\/football\/slideshow\/20614187_1114381958662883_1250436784_n.jpg","..\/images\/leaflet\/football\/slideshow\/20614292_1114381905329555_1610821727_n.jpg","..\/images\/leaflet\/football\/slideshow\/20615121_1114388431995569_1469184180_o.jpg"];
+    $(document).ready(function() {
+        var slideshow = new Slideshow(document.getElementById("slideshow"), files);
+    });
+</script>
 </body>
 </html>
---
+++ live/static/slideshow.js
@@ -0,0 +1,94 @@
+function Slideshow(container, imagesPaths) {
+    var slideshow = this;
+
+    this.container = container;
+    this.imagesPaths = imagesPaths;
+
+    this.currentIndex = 0;
+    this.isPlaying = false;
+
+    this.container.classList.add("slideshow"); // add class to container
+
+    // image container
+    this.imageContainer = document.createElement("div");
+    this.imageContainer.classList.add("slideshowImageContainer");
+    // image
+    this.image = document.createElement("img");
+    this.updateImage(this.imagesPaths[this.currentIndex]);
+    this.imageContainer.appendChild(this.image);
+    // add image container
+    this.container.appendChild(this.imageContainer);
+
+    // control
+    this.controlContainer = document.createElement("div");
+    this.controlContainer.classList.add("slideshowControl");
+    // previous button
+    this.previousButton = document.createElement("button");
+    this.previousButton.textContent = '<';
+    this.previousButton.classList.add("slideshowButton");
+    this.previousButton.addEventListener("click", function(){slideshow.nextPhoto(-1)});
+    this.controlContainer.appendChild(this.previousButton);
+    // pause button
+    this.pauseButton = document.createElement("button");
+    this.pauseButton.textContent = '||';
+    this.pauseButton.classList.add("slideshowButton");
+    this.pauseButton.addEventListener("click", function(){slideshow.pausePlay()});
+    this.controlContainer.appendChild(this.pauseButton);
+    // next button
+    this.nextButton = document.createElement("button");
+    this.nextButton.textContent = '>';
+    this.nextButton.classList.add("slideshowButton");
+    this.nextButton.addEventListener("click", function(){slideshow.nextPhoto(1)});
+    this.controlContainer.appendChild(this.nextButton);
+    // add control
+    this.container.appendChild(this.controlContainer);
+
+    // start
+    this.startSlideshow();
+}
+
+Slideshow.prototype.updateImage = function(imagePath) {
+    this.image.src = imagePath;
+    this.image.alt = "";
+}
+
+Slideshow.prototype.startSlideshow = function() {
+    this.isPlaying = true;
+    var slideshow = this;
+    this.autoSlideshow = setInterval(function() {
+        slideshow.nextPhoto(1);
+    }, 5000);
+}
+
+Slideshow.prototype.nextPhoto = function(skip) {
+    this.currentIndex += skip;
+
+    if(this.currentIndex < 0){
+        this.currentIndex = this.imagesPaths.length + this.currentIndex;
+    }
+
+    if(this.currentIndex >= this.imagesPaths.length){
+        this.currentIndex = this.currentIndex - this.imagesPaths.length;
+    }
+
+    this.updateImage(this.imagesPaths[this.currentIndex]);
+
+    if (this.isPlaying) {
+        // reset timer
+        clearInterval(this.autoSlideshow);
+        this.startSlideshow();
+    }
+}
+
+Slideshow.prototype.pausePlay = function(button) {
+    if (this.isPlaying) {
+        clearInterval(this.autoSlideshow);
+        this.isPlaying = false;
+        this.pauseButton.classList.add("slideshowButtonToggled");
+    } else {
+        clearInterval(this.autoSlideshow);
+        this.startSlideshow();
+        this.isPlaying = true;
+        this.pauseButton.classList.remove("slideshowButtonToggled");
+    }
+}
