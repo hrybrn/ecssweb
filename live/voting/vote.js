65273bb86de07011e31cf98086e1ccab05d6d00b
Unknown <harryjgbrown@gmail.com>
2017-10-07 00:32:47 +0100

    centred the headers

--- live/voting/vote.js
+++ live/voting/vote.js
@@ -26,7 +26,7 @@ function showPosition(buttonid) {
 			}
 
 			$.each(nominations.data, function () {
-				var div = "<div id='" + this.nominationID + "' class='group'><h3>" + this.nominationName + "</h3>";
+				var div = "<div id='" + this.nominationID + "' class='group'><h3 style='text-align: center;'>" + this.nominationName + "</h3>";
 				div += "<div data-nominationid='" + this.nominationID + "'><p>" + this.manifesto + "</p>";
 				div += "<image src='/nominations/" + this.image + "'</div></div>";
 
