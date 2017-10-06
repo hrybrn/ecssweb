b025cab75896e59c0dc300d49aadc8dc02774008
Unknown <harryjgbrown@gmail.com>
2017-10-07 00:21:06 +0100

    more touch fixes

--- live/voting/vote.js
+++ live/voting/vote.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 	showPosition(first);
+	init();
 });
 
 var entryData = {};
@@ -90,4 +91,28 @@ function submit() {
 			}
 		}
 	});
+}
+
+function touchHandler(event) {
+    var touch = event.changedTouches[0];
+
+    var simulatedEvent = document.createEvent("MouseEvent");
+        simulatedEvent.initMouseEvent({
+        touchstart: "mousedown",
+        touchmove: "mousemove",
+        touchend: "mouseup"
+    }[event.type], true, true, window, 1,
+        touch.screenX, touch.screenY,
+        touch.clientX, touch.clientY, false,
+        false, false, false, 0, null);
+
+    touch.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
+}
+
+function init() {
+    document.addEventListener("touchstart", touchHandler, true);
+    document.addEventListener("touchmove", touchHandler, true);
+    document.addEventListener("touchend", touchHandler, true);
+    document.addEventListener("touchcancel", touchHandler, true);
 }
