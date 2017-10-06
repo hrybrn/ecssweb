df37a220e30a103a3c7c0031ed1fb3af58669ba1
Unknown <harryjgbrown@gmail.com>
2017-10-07 00:49:15 +0100

    drag on the logos now works!!

--- live/voting/vote.js
+++ live/voting/vote.js
@@ -26,7 +26,7 @@ function showPosition(buttonid) {
 			}
 
 			$.each(nominations.data, function () {
-				var div = "<div id='" + this.nominationID + "' class='group'><h3 style='text-align: center;'>" + this.nominationName + "</h3>";
+				var div = "<div id='" + this.nominationID + "' class='group'><h3>" + this.nominationName + "<img class='dragme'></h3>";
 				div += "<div data-nominationid='" + this.nominationID + "'><p>" + this.manifesto + "</p>";
 				div += "<image src='/nominations/" + this.image + "'</div></div>";
 
@@ -54,7 +54,7 @@ function showPosition(buttonid) {
 				autoheight: false
 			}).sortable({
 				axis: "y",
-				handle: "h3",
+				handle: ".dragme",
 				stop: function( event, ui ) {
 				  ui.item.children( "h3" ).triggerHandler( "focusout" );
 		 
