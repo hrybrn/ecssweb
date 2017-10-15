0e45394f096a2155f3a623c5b3d89648de2e8d48
Unknown <harryjgbrown@gmail.com>
2017-10-15 13:52:44 +0100

    charity election results

--- live/voting/results/index.php
+++ live/voting/results/index.php
@@ -2,60 +2,6 @@
 $relPath = "../../";
 include_once ($relPath . 'includes/setLang.php');
 
-$dbLoc = realpath($relPath . "../db/ecss.db");
-
-$db = new PDO('sqlite:' . $dbLoc);
-
-require_once($relPath . '../config/config.php');
-
-if (DEBUG) {
-    //debug version
-    $attributes = [
-    	"http://schemas.microsoft.com/ws/2008/06/identity/claims/windowsaccountname" => array("hb15g16"),
-    	"http://schemas.xmlsoap.org/claims/Group" => array(
-    		"Domain Users",
-    		"allStudent",
-    		"fpStudent",
-    		"jfNISSync",
-    		"fpappvmatlab2009b",
-    		"AllStudentsMassEmail",
-    		"f7_All_Faculty_Student",
-    		"ebXRDDataSharedResourceRead",
-    		"isiMUSI2015Users",
-    		"jfSW_Deploy_OpenChoiceDesktop_2.2_SCCM"
-    		),
-    	"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress" => array("hb15g16@ecs.soton.ac.uk"),
-    	"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname" => array("Harry"),
-    	"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname" => array("Brown")
-    ];
-} else {
-    //live version
-    require_once('/var/www/auth/lib/_autoload.php');
-    $as = new SimpleSAML_Auth_Simple('default-sp');
-    $as->requireAuth();
-    $attributes = $as->getAttributes();
-}
-
-// csrf token
-session_name('csrf_protection');
-session_start();
-if (empty($_SESSION['csrftoken'])) {
-    $_SESSION['csrftoken'] = bin2hex(random_bytes(32));
-}
-$csrftoken = $_SESSION['csrftoken'];
-
-$userInfo = array(
-	'username' => $attributes["http://schemas.microsoft.com/ws/2008/06/identity/claims/windowsaccountname"][0],
-	'groups' => $attributes["http://schemas.xmlsoap.org/claims/Group"],
-	'email' => $attributes["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"][0],
-	'firstName' => $attributes["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname"][0],
-	'lastName' => $attributes["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname"][0]
-);
-
-if(true){
-    echo json_encode(['status' => false, 'message' => "You're not a member of ECS"]);
-    exit;
-}
 ?>
 <!doctype html>
 <html>
@@ -68,35 +14,85 @@ if(true){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrftoken" content="<?= $csrftoken ?>">
     <link rel="stylesheet" type="text/css" href="<?= $relPath ?>theme.css" />
+    <link rel="stylesheet" type="text/css" href="/voting/results/results.css" />
 </head>
 <body>
-<script src="/voting/results/results.js" type="text/javascript"></script>
 <script src="/jquery.js" type="text/javascript"></script>
-
-<script type="text/javascript">
-    $(document).ready(function(){
-        load();
-    });
-</script>
 
 <?php
 include_once($relPath . "navbar/navbar.php");
 echo getNavBar();
+?>
 
-$electionID = 1;
+<h3>Final Results of the Charity Election</h3>
 
-$sql = "SELECT n.nominationName, v.ranking
-        FROM vote AS v
-        INNER JOIN nomination AS n
-        ON v.nominationID = n.nominationID
-        WHERE n.positionID = 3
-        AND n.electionID = 1;";
-$res = "";
-$statement = $db->query($sql);
-while($row = $statement->fetchObject()){
-    $res .= $row->nominationName . "," . $row->ranking . "\n";
-}
+<span id='currentRound'></span>
+<div id='buttonDiv'>
+    <button id='previous'>Previous Round</button>
+    <button id='next'>See The Rundown</button>
+    <button id='final'>Final Results</button>
+</div>
 
-$csv = fopen("results.csv", 'w');
+<script type="text/javascript">
+    var results = [];
 
-fwrite($csv, $res);
+    results[0] = "<p>Number of votes cast: 90<br />Candidates were:</p><p>You're It Southampton<br />Get Well Gamers<br />Solent Mind<br />S.C.R.A.T.C.H<br />Special Effect<br />Autism Hampshire<br />Mencap<br />Rowans Hospice<br />Practical Action</p>";
+
+    results[1] = "<p>-----<br />ROUND 1<br />-----<br />You're It Southampton : 17<br />Solent Mind : 13<br />Autism Hampshire : 11<br />Get Well Gamers : 10<br />S.C.R.A.T.C.H : 9<br />Rowans Hospice : 9<br />Practical Action : 8<br />Special Effect : 7<br />Mencap : 6</p><p>Mencap has been eliminated</p>";
+
+    results[2] = "<p>-----<br />ROUND 2<br />-----<br />You're It Southampton : 17<br />Solent Mind : 13<br />S.C.R.A.T.C.H : 12<br />Autism Hampshire : 12<br />Get Well Gamers : 11<br />Rowans Hospice : 9<br />Special Effect : 8<br />Practical Action : 8</p><p>Special Effect has been eliminated</p>";
+
+    results[3] = "<p>-----<br />ROUND 3<br />-----<br />You're It Southampton : 19<br />Solent Mind : 16<br />S.C.R.A.T.C.H : 13<br />Autism Hampshire : 13<br />Get Well Gamers : 11<br />Rowans Hospice : 9<br />Practical Action : 9</p><p>Practical Action has been eliminated</p>";
+
+    results[4] = "<p>-----<br />ROUND 4<br />-----<br />You're It Southampton : 19<br />Solent Mind : 19<br />S.C.R.A.T.C.H : 16<br />Get Well Gamers : 13<br />Autism Hampshire : 13<br />Rowans Hospice : 10</p><p>Rowans Hospice has been eliminated</p>";
+
+    results[5] = "<p>-----<br />ROUND 5<br />-----<br />You're It Southampton : 24<br />Solent Mind : 21<br />S.C.R.A.T.C.H : 17<br />Autism Hampshire : 15<br />Get Well Gamers : 13</p><p>Get Well Gamers has been eliminated</p>";
+
+    results[6] = "<p>-----<br />ROUND 6<br />-----<br />You're It Southampton : 28<br />Solent Mind : 24<br />S.C.R.A.T.C.H : 20<br />Autism Hampshire : 18</p><p>Autism Hampshire has been eliminated</p>";
+
+    results[7] = "<p>-----<br />ROUND 7<br />-----<br />You're It Southampton : 35<br />Solent Mind : 31<br />S.C.R.A.T.C.H : 24</p><p>S.C.R.A.T.C.H has been eliminated</p>";
+
+    results[8] = "<p>-----<br />ROUND 8<br />-----<br />You're It Southampton : 48<br />Solent Mind : 42</p><p>Solent Mind has been eliminated</p><p>-----<br />WINNER IS: You're It Southampton<br />-----</p>";
+
+    var final = 8;
+    var current = -1;
+
+
+    $('#currentRound').html(results[8]);
+
+    $('#previous').hide();
+    $('#final').hide();
+
+    $(document).ready(function(){
+        $('#previous').click(function(){
+            changeRound(-1);
+        });
+
+        $('#next').click(function(){
+            changeRound(1);
+            $('#previous').show();
+            $('#final').show();
+            $('#next').html("Next Round");
+        });
+
+        $('#final').click(function(){
+            setRound(final);
+            current = final;
+        })
+    });
+
+    function changeRound(increment){
+        var next = current + increment;
+
+        if(next >= 0 && next <= final){
+            current = next;
+            setRound(current);
+        }
+    }
+
+    function setRound(roundNumber){
+        if(roundNumber >= 0 && roundNumber <= final){
+            $('#currentRound').html(results[roundNumber]);
+        }
+    }
+</script>
